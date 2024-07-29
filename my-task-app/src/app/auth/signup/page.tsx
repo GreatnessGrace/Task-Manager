56b21d812465408fa5fa356d98ddc9ce49a3043f
GreatnessGrace <graceunstoppable7@gmail.com>
2024-07-29 17:30:10 +0530

    Signin Page with tailwind

--- my-task-app/src/app/auth/signup/page.tsx
+++ my-task-app/src/app/auth/signup/page.tsx
@@ -17,14 +17,15 @@ const SignupPage: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-blue-100 to-blue-200">
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-purple-100 to-purple-200">
       <div className="bg-white p-10 rounded-lg shadow-lg w-96 text-center">
-        <h2 className="text-2xl font-semibold mb-6">Welcome to <span className="text-purple-600">Workflo!</span></h2>
+        <h2 className="text-2xl font-semibold mb-6">Welcome to <span className="text-blue-800">Workflo!</span></h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label className="block text-left mb-2 text-sm font-medium text-gray-600">Full name:</label>
+            {/* <label className="block text-left mb-2 text-sm font-medium text-gray-600">Full name:</label> */}
             <input
               type="text"
+              placeholder='Full name'
               value={name}
               onChange={(e) => setName(e.target.value)}
               required
@@ -32,9 +33,10 @@ const SignupPage: React.FC = () => {
             />
           </div>
           <div className="mb-4">
-            <label className="block text-left mb-2 text-sm font-medium text-gray-600">Your email:</label>
+            {/* <label className="block text-left mb-2 text-sm font-medium text-gray-600">Your email:</label> */}
             <input
               type="email"
+              placeholder='Your email'
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
@@ -42,9 +44,10 @@ const SignupPage: React.FC = () => {
             />
           </div>
           <div className="mb-6">
-            <label className="block text-left mb-2 text-sm font-medium text-gray-600">Password:</label>
+            {/* <label className="block text-left mb-2 text-sm font-medium text-gray-600">Password:</label> */}
             <input
               type="password"
+              placeholder='Password'
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
@@ -53,13 +56,13 @@ const SignupPage: React.FC = () => {
           </div>
           <button
             type="submit"
-            className="w-full bg-purple-600 text-white py-2 rounded-lg font-medium hover:bg-purple-700 transition duration-200"
+            className="w-full bg-purple-300 text-white py-2 rounded-lg font-medium hover:bg-purple-700 transition duration-200"
           >
             Sign up
           </button>
         </form>
         <p className="mt-6 text-sm text-gray-600">
-          Already have an account? <a href="/auth/login" className="text-purple-600 hover:underline">Log in.</a>
+          Already have an account? <a href="/auth/login" className="text-blue-600 hover:underline">Log in.</a>
         </p>
       </div>
     </div>
