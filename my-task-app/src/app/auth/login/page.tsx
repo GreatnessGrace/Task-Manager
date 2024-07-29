2980f77bac955aa4f9ee524b4bef24a29894cbe9
GreatnessGrace <graceunstoppable7@gmail.com>
2024-07-29 17:48:51 +0530

    react icon in login page

--- my-task-app/src/app/auth/login/page.tsx
+++ my-task-app/src/app/auth/login/page.tsx
@@ -3,10 +3,12 @@
 import React, { useState } from 'react';
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai'; // Import icons
 
 const LoginPage: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false); // State to toggle password visibility
   const router = useRouter();
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -40,16 +42,23 @@ const LoginPage: React.FC = () => {
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-gray-400 bg-gray-100"
             />
           </div>
-          <div className="mb-6">
+          <div className="mb-6 relative"> {/* Add relative positioning */}
             {/* <label className="block text-left mb-2 text-sm font-medium text-gray-600">Password:</label> */}
             <input
-              type="password"
+              type={showPassword ? 'text' : 'password'} // Toggle input type
               value={password}
               placeholder='Password'
               onChange={(e) => setPassword(e.target.value)}
               required
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-gray-400 bg-gray-100"
             />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)} // Toggle password visibility
+              className="absolute inset-y-0 right-0 flex items-center px-3 text-gray-400"
+            >
+              {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
+            </button>
           </div>
           <button
             type="submit"
