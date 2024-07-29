9c0dcda0ca2b74892544d3154f43d75708eb562a
GreatnessGrace <graceunstoppable7@gmail.com>
2024-07-29 17:29:54 +0530

    Login Page with tailwind

--- my-task-app/src/app/auth/login/page.tsx
+++ my-task-app/src/app/auth/login/page.tsx
@@ -25,25 +25,27 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-blue-100 to-blue-200">
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-purple-100 to-purple-200">
       <div className="bg-white p-10 rounded-lg shadow-lg w-96 text-center">
         <h2 className="text-2xl font-semibold mb-6">Welcome to <span className="text-purple-600">Workflo!</span></h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label className="block text-left mb-2 text-sm font-medium text-gray-600">Email:</label>
+            {/* <label className="block text-left mb-2 text-sm font-medium text-gray-600">Email:</label> */}
             <input
               type="email"
               value={email}
+              placeholder='Your email'
               onChange={(e) => setEmail(e.target.value)}
               required
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-purple-600"
             />
           </div>
           <div className="mb-6">
-            <label className="block text-left mb-2 text-sm font-medium text-gray-600">Password:</label>
+            {/* <label className="block text-left mb-2 text-sm font-medium text-gray-600">Password:</label> */}
             <input
               type="password"
               value={password}
+              placeholder='Password'
               onChange={(e) => setPassword(e.target.value)}
               required
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-purple-600"
