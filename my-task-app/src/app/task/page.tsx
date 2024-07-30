3e451e291161dc6957a0adf0d8db4e5c459e4653
GreatnessGrace <graceunstoppable7@gmail.com>
2024-07-30 13:00:40 +0530

    help & feedback icon update

--- my-task-app/src/app/task/page.tsx
+++ my-task-app/src/app/task/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Sidebar from '../components/Sidebar';
 import TaskBoard from '../components/TaskBoard';
 import TaskCards from '../components/TaskCards';
+import { CiCircleQuestion } from "react-icons/ci";
 
 const TaskPage: React.FC = () => {
   return (
@@ -10,7 +11,7 @@ const TaskPage: React.FC = () => {
       <div className="flex-1 p-6 bg-gray-100">
         <header className="flex justify-between items-center mb-8">
           <h1 className="text-3xl font-semibold">Good morning, Joe!</h1>
-          <button className="text-gray-800 font-semibold">Help & feedback</button>
+          <button className="text-gray-800 font-semibold flex flex-center justify-center">Help & feedback <CiCircleQuestion className='font-semibold text-xl mt-1 ml-1'/></button>
         </header>
         <TaskCards />
         <TaskBoard />
