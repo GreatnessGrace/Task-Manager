1f1f8489f73158f934a2025f6432f4530afd573c
GreatnessGrace <graceunstoppable7@gmail.com>
2024-07-30 10:26:44 +0530

    Task Board icon & Task Update

--- my-task-app/src/app/components/TaskBoard.tsx
+++ my-task-app/src/app/components/TaskBoard.tsx
@@ -59,6 +59,7 @@
 
 import React from 'react';
 import TaskColumn from './TaskColumn';
+import { CiCirclePlus } from "react-icons/ci";
 
 const TaskBoard: React.FC = () => {
   return (
@@ -71,13 +72,16 @@ const TaskBoard: React.FC = () => {
           <button className="px-4 py-2 bg-white rounded-lg shadow-md">Filter</button>
           <button className="px-4 py-2 bg-white rounded-lg shadow-md">Share</button>
         </div>
-        <button className="px-4 py-2 bg-purple-600 text-white rounded-lg shadow-md">Create new</button>
+        {/* <div className="flex justify-between items-center mb-4"> */}
+
+        <button className="px-4 py-2 bg-purple-600 text-white rounded-lg shadow-md flex items-center justify-between">Create new &nbsp;<CiCirclePlus className='bg-white text-black rounded-lg'/>
+        </button>
       </div>
       <div className="flex space-x-4">
         <TaskColumn title="To do" tasks={[{ id: 1, title: 'Implement User Authentication', priority: 'Urgent', date: '2024-08-15', time: '1 hr ago' }, /* more tasks */]} />
-        <TaskColumn title="In progress" tasks={[{ id: 2, title: 'Design Home Page UI', priority: 'Medium', date: '2024-08-15', time: '1 hr ago' }, /* more tasks */]} />
-        <TaskColumn title="Under review" tasks={[{ id: 3, title: 'Integrate Cloud Storage', priority: 'Urgent', date: '2024-08-20', time: '2 days ago' }, /* more tasks */]} />
-        <TaskColumn title="Finished" tasks={[{ id: 4, title: 'Test Cross-browser Compatibility', priority: 'Medium', date: '2024-07-30', time: '4 days ago' }, /* more tasks */]} />
+        <TaskColumn title="In progress" tasks={[{ id: 2, title: 'Design Home Page UI', priority: 'Medium', date: '2024-08-15', time: '1 hr ago' },{ id: 3, title: 'Conduct User Feedback Survey', priority: 'Low', date: '2024-08-15', time: '3 hr ago' }, /* more tasks */]} />
+        <TaskColumn title="Under review" tasks={[{ id: 4, title: 'Integrate Cloud Storage', priority: 'Urgent', date: '2024-08-20', time: '2 days ago' }, /* more tasks */]} />
+        <TaskColumn title="Finished" tasks={[{ id: 5, title: 'Test Cross-browser Compatibility', priority: 'Medium', date: '2024-07-30', time: '4 days ago' }, /* more tasks */]} />
       </div>
     </div>
   );
