aa1cbe16362b4afe00bb16d35635465dc341f911
GreatnessGrace <graceunstoppable7@gmail.com>
2024-07-30 10:27:04 +0530

    title icon

--- my-task-app/src/app/components/TaskColumn.tsx
+++ my-task-app/src/app/components/TaskColumn.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai'; // Import icons
+import { RiMenuFold2Fill } from "react-icons/ri";
 
 interface Task {
   id: number;
@@ -23,10 +25,15 @@ const TaskColumn: React.FC<TaskColumnProps> = ({ title, tasks }) => {
 
   return (
     <div className="flex-1 bg-white p-4 rounded-lg shadow-md">
-      <h2 className="text-xl font-semibold mb-4">{title}</h2>
+  
+            <div className="flex justify-between items-center mb-4">
+
+       <h2 className="text-xl font-semibold">{title}</h2>
+       <RiMenuFold2Fill className="h-6 w-6 text-gray-400" />  
+       </div>
       <div className="space-y-4">
         {tasks.map((task) => (
-          <div key={task.id} className="bg-gray-100 p-4 rounded-lg shadow-md">
+          <div key={task.id} className="bg-gray-100 p-4 rounded-lg shadow-md " >
             <h3 className="text-lg font-semibold">{task.title}</h3>
             <button className={`text-sm px-2 rounded-lg ${priorityColors[task.priority]}`}>{task.priority}</button>
             {/* <button className="text-sm bg-red-400  px-2 rounded-lg text-gray-500">{task.priority}</button> */}
