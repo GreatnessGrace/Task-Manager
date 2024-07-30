d3a8ef0c1124721583d41eebf90682e22f589bea
GreatnessGrace <graceunstoppable7@gmail.com>
2024-07-30 10:08:58 +0530

    priority button dynamic color

--- my-task-app/src/app/components/TaskColumn.tsx
+++ my-task-app/src/app/components/TaskColumn.tsx
@@ -14,6 +14,13 @@ interface TaskColumnProps {
 }
 
 const TaskColumn: React.FC<TaskColumnProps> = ({ title, tasks }) => {
+    const priorityColors: { [key: string]: string } = {
+        Urgent: 'bg-red-400 text-white',
+        High: 'bg-orange-400 text-white',
+        Medium: 'bg-yellow-400 text-black',
+        Low: 'bg-green-400 text-white',
+      };
+
   return (
     <div className="flex-1 bg-white p-4 rounded-lg shadow-md">
       <h2 className="text-xl font-semibold mb-4">{title}</h2>
@@ -21,12 +28,13 @@ const TaskColumn: React.FC<TaskColumnProps> = ({ title, tasks }) => {
         {tasks.map((task) => (
           <div key={task.id} className="bg-gray-100 p-4 rounded-lg shadow-md">
             <h3 className="text-lg font-semibold">{task.title}</h3>
-            <p className="text-sm text-gray-500">{task.priority}</p>
+            <button className={`text-sm px-2 rounded-lg ${priorityColors[task.priority]}`}>{task.priority}</button>
+            {/* <button className="text-sm bg-red-400  px-2 rounded-lg text-gray-500">{task.priority}</button> */}
             <p className="text-sm text-gray-500">{task.date}</p>
             <p className="text-sm text-gray-500">{task.time}</p>
           </div>
         ))}
-        <button className="w-full bg-gray-200 text-gray-700 py-2 rounded-lg shadow-md">Add new</button>
+        <button className="w-full bg-black text-white py-2 rounded-lg shadow-md">Add new</button>
       </div>
     </div>
   );
