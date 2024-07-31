494bbebcacece76939e8aef8301db938210c9da3
GreatnessGrace <graceunstoppable7@gmail.com>
2024-07-31 20:08:12 +0530

    updated

--- my-task-app/src/pages/api/signup.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { createUser } from '../../utils/db';
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { email, password } = req.body;
-  await createUser(email, password);
-  res.status(201).end();
-};
